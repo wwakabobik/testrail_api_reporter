f7e24778a221fd7dd0a3821cb60bc9001724e22f
wwakabobik <i.vereshchagin@gmail.com>
2022-09-14 16:27:53 +0200

    Add slack sender functionality

--- email_sender.py
+++ email_sender.py
@@ -1,5 +1,6 @@
-import smtplib
 from datetime import datetime
+
+import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
