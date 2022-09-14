ae33ebcee7f69f5d478cf55fafa5ccc126223a39
wwakabobik <i.vereshchagin@gmail.com>
2022-09-14 19:34:14 +0200

    Add separate inits

---
+++ testrail_api_reporter/engines/__init__.py
@@ -0,0 +1,4 @@
+# Engines
+from .at_coverage_reporter import ATCoverageReporter
+from .results_reporter import TestRailResultsReporter
+from .plotly_reporter import PlotlyReporter
---
+++ testrail_api_reporter/publishers/__init__.py
@@ -0,0 +1,4 @@
+# Publishers
+from .confluence_sender import ConfluenceSender
+from .email_sender import EmailSender
+from .slack_sender import SlackSender
