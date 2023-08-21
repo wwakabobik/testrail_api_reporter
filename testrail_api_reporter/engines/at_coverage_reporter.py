0eec3e8eae55230069cb1a591622c6a067406ad6
Ilya Vereshchagin <i.vereshchagin@gmail.com>
2023-08-21 12:05:05 +0200

    1.3.3: fix offset obtaining (#31)
    * Fix obtaining offset value
    
    * Add GA for linters
    
    * Add GA for linters
    
    * Add GA for linters
    
    * Add GA for linters
    
    * Linters fixes
    
    * Linters fixes

--- testrail_api_reporter/engines/at_coverage_reporter.py
+++ testrail_api_reporter/engines/at_coverage_reporter.py
@@ -1,4 +1,5 @@
-from testrail_api import TestRailAPI
+""" Engine to generate obtain TestRail data and prepare reports """
+from testrail_api import TestRailAPI  # type: ignore
 
 from ..utils.case_stat import CaseStat
 from ..utils.csv_parser import CSVParser
@@ -10,8 +11,18 @@ class ATCoverageReporter:
     Class for generate data for automation coverage reports (or similar data) from TestRails
     """
 
-    def __init__(self, url: str, email: str, password: str, priority=None, project=None, type_platforms=None,
-                 automation_platforms=None, suite_id=None, debug=None):
+    def __init__(
+        self,
+        url: str,
+        email: str,
+        password: str,
+        priority=None,
+        project=None,
+        type_platforms=None,
+        automation_platforms=None,
+        suite_id=None,
+        debug=None,
+    ):
         """
         General init
 
@@ -35,8 +46,6 @@ class ATCoverageReporter:
             print("\nAT Coverage Reporter init")
         if url is None or email is None or password is None:
             raise ValueError("No TestRails credentials are provided!")
-        else:
-            pass
         self.__debug = debug if debug is not None else True
         self.__automation_platforms = automation_platforms  # should be passed with specific TestRails sections
         self.__type_platforms = type_platforms  # should be passed with specific TestRails sections
@@ -60,8 +69,8 @@ class ATCoverageReporter:
             raise ValueError("No project specified, report aborted!")
         all_sections = self.__get_all_sections(project_id=project, suite_id=suite_id)
         for section in all_sections:
-            if section['parent_id'] in parent_list:
-                parent_list.append(section['id'])
+            if section["parent_id"] in parent_list:
+                parent_list.append(section["id"])
         return parent_list
 
     def __get_all_sections(self, project_id=None, suite_id=None, debug=None):
@@ -86,20 +95,31 @@ class ATCoverageReporter:
             if first_run:
                 try:
                     response = self.__api.sections.get_sections(project_id=project, suite_id=suite_id)
-                except Exception as e:
-                    print(f"Get sections failed. Please validate your settings!\nError{format_error(e)}")
+                except Exception as error:  # pylint: disable=broad-except
+                    print(f"Get sections failed. Please validate your settings!\nError{format_error(error)}")
                     return None
                 first_run = False
-            elif response['_links']['next'] is not None:
-                response = self.__api.sections.get_sections(project_id=project, suite_id=suite_id,
-                                                            offset=int(response['_links']['next'].split("&offset=")[1]))
-            sections = sections + response['sections']
-            criteria = response['_links']['next']
+            elif response["_links"]["next"] is not None:  # pylint: disable=unsubscriptable-object
+                offset = int(
+                    response["_links"]["next"]  # pylint: disable=unsubscriptable-object
+                    .partition("offset=")[2]
+                    .partition("&")[0]
+                )
+                response = self.__api.sections.get_sections(project_id=project, suite_id=suite_id, offset=offset)
+            sections = sections + response["sections"]  # pylint: disable=unsubscriptable-object
+            criteria = response["_links"]["next"]  # pylint: disable=unsubscriptable-object
         if debug:
-            print(f'Found {len(sections)} existing sections in TestRails for project {project_id}, suite {suite_id}')
+            print(f"Found {len(sections)} existing sections in TestRails for project {project_id}, suite {suite_id}")
         return sections
 
-    def __get_all_cases(self, project_id=None, suite_id=None, section_id=None, priority_id=None, debug=None):
+    def __get_all_cases(
+        self,
+        project_id=None,
+        suite_id=None,
+        section_id=None,
+        priority_id=None,
+        debug=None,
+    ):
         """
         Wrapper to get all test cases for selected project, suite, section and priority
 
@@ -120,25 +140,49 @@ class ATCoverageReporter:
         while criteria is not None or first_run:
             if first_run:
                 try:
-                    response = self.__api.cases.get_cases(project_id=project_id, suite_id=suite_id,
-                                                          section_id=section_id, priority_id=priority_id)
-                except Exception as e:
-                    raise ValueError(f"Get cases failed. Please validate your settings!\nError{format_error(e)}")
+                    response = self.__api.cases.get_cases(
+                        project_id=project_id,
+                        suite_id=suite_id,
+                        section_id=section_id,
+                        priority_id=priority_id,
+                    )
+                except Exception as error:  # pylint: disable=broad-except
+                    raise ValueError(
+                        f"Get cases failed. Please validate your settings!\nError{format_error(error)}"
+                    ) from error
                 first_run = False
-            elif response['_links']['next'] is not None:
-                offset = int(response['_links']['next'].split("&offset=")[1].split("&")[0])
-                response = self.__api.cases.get_cases(project_id=project_id, suite_id=suite_id,
-                                                      section_id=section_id, priority_id=priority_id, offset=offset)
-            cases_list = cases_list + response['cases']
-            criteria = response['_links']['next']
+            elif response["_links"]["next"] is not None:  # pylint: disable=unsubscriptable-object
+                offset = int(
+                    response["_links"]["next"]  # pylint: disable=unsubscriptable-object
+                    .partition("offset=")[2]
+                    .partition("&")[0]
+                )
+                response = self.__api.cases.get_cases(
+                    project_id=project_id,
+                    suite_id=suite_id,
+                    section_id=section_id,
+                    priority_id=priority_id,
+                    offset=offset,
+                )
+            cases_list = cases_list + response["cases"]  # pylint: disable=unsubscriptable-object
+            criteria = response["_links"]["next"]  # pylint: disable=unsubscriptable-object
 
         if debug:
-            print(f'Found {len(cases_list)} existing tests in TestRails for project {project_id}, suite {suite_id}, '
-                  f'section {section_id}, priority {priority_id}')
+            print(
+                f"Found {len(cases_list)} existing tests in TestRails for project {project_id}, suite {suite_id}, "
+                f"section {section_id}, priority {priority_id}"
+            )
         return cases_list
 
-    def automation_state_report(self, priority=None, project=None, automation_platforms=None,
-                                filename_pattern='current_automation', suite=None, debug=None):
+    def automation_state_report(
+        self,
+        priority=None,
+        project=None,
+        automation_platforms=None,
+        filename_pattern="current_automation",
+        suite=None,
+        debug=None,
+    ):
         """
         Generates data of automation coverage for stacked bar chart or staked line chart
         with values "Automated", "Not automated", "N/A". This distribution generated using values form fields
@@ -168,28 +212,33 @@ class ATCoverageReporter:
             raise ValueError("No automation platforms specified, report aborted!")
         debug = debug if debug is not None else self.__debug
         if debug:
-            print('=== Staring generation of report for current automation state ===')
+            print("=== Staring generation of report for current automation state ===")
         index = 0
         results = []
         for platform in automation_platforms:
             if debug:
-                print('processing platform ' + platform['name'])
-            results.append(CaseStat(platform['name']))
-            sections = self.__get_sections(platform['sections'])
+                print("processing platform " + platform["name"])
+            results.append(CaseStat(platform["name"]))
+            sections = self.__get_sections(platform["sections"])
             for section in sections:
                 if debug:
-                    print('passing section ' + str(section))
-                cases = self.__get_all_cases(project_id=project, suite_id=suite, section_id=section,
-                                             priority_id=priority)
+                    print("passing section " + str(section))
+                cases = self.__get_all_cases(
+                    project_id=project,
+                    suite_id=suite,
+                    section_id=section,
+                    priority_id=priority,
+                )
                 results[index].set_total(results[index].get_total() + len(cases))
                 for case in cases:
-                    if case[platform['internal_name']] == platform['auto_code']:
+                    if case[platform["internal_name"]] == platform["auto_code"]:
                         results[index].set_automated(results[index].get_automated() + 1)
                     else:
-                        if case[platform['internal_name']] == platform['na_code']:
+                        if case[platform["internal_name"]] == platform["na_code"]:
                             results[index].set_na(results[index].get_na() + 1)
-            results[index].set_not_automated(results[index].get_total() - results[index].get_automated() -
-                                             results[index].get_na())
+            results[index].set_not_automated(
+                results[index].get_total() - results[index].get_automated() - results[index].get_na()
+            )
             # save history data
             filename = f"{filename_pattern}_{results[index].get_name().replace(' ', '_')}.csv"
             CSVParser(debug=debug, filename=filename).save_history_data(report=results[index])
@@ -211,16 +260,22 @@ class ATCoverageReporter:
         if not project:
             raise ValueError("No project specified, report aborted!")
         if debug:
-            print('=== Staring generation of report for test case priority distribution ===')
+            print("=== Staring generation of report for test case priority distribution ===")
         results = []
         for i in range(1, 5):
             if debug:
-                print(f'passing priority {str(i)}')
+                print(f"passing priority {str(i)}")
             results.append(len(self.__get_all_cases(project_id=project, suite_id=suite, priority_id=str(i))))
         return results
 
-    def test_case_by_type(self, project=None, type_platforms=None, filename_pattern='current_area_distribution',
-                          suite=None, debug=None):
+    def test_case_by_type(
+        self,
+        project=None,
+        type_platforms=None,
+        filename_pattern="current_area_distribution",
+        suite=None,
+        debug=None,
+    ):
         """
         Generates data for pie/line chart with distribution by type of platforms (guided by top section).
 
@@ -242,17 +297,17 @@ class ATCoverageReporter:
         debug = debug if debug is not None else self.__debug
         project = project if project else self.__project
         if debug:
-            print('=== Staring generation of report for test case area distribution ===')
+            print("=== Staring generation of report for test case area distribution ===")
         index = 0
         results = []
         for platform in type_platforms:
             if debug:
-                print('processing area ' + platform['name'])
-            results.append(CaseStat(platform['name']))
-            sections = self.__get_sections(platform['sections'])
+                print("processing area " + platform["name"])
+            results.append(CaseStat(platform["name"]))
+            sections = self.__get_sections(platform["sections"])
             for section in sections:
                 if debug:
-                    print('passing section ' + str(section))
+                    print("passing section " + str(section))
                 cases = self.__get_all_cases(project_id=project, suite_id=suite, section_id=section)
                 results[index].set_total(results[index].get_total() + len(cases))
             # save history data
