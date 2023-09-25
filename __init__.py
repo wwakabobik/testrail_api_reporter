d7ac62f5ff0996b92da8c83fa8f3c065f116c54e
wwakabobik <i.vereshchagin@gmail.com>
2023-09-25 17:46:27 +0200

    Get back main and init py for package

--- __init__.py
+++ __init__.py
@@ -1,4 +1,11 @@
 """ This module is used to import all the classes and functions from the package """
 # Engines
+from .testrail_api_reporter.engines.at_coverage_reporter import ATCoverageReporter
+from .testrail_api_reporter.engines.results_reporter import TestRailResultsReporter
+from .testrail_api_reporter.engines.plotly_reporter import PlotlyReporter
 # Publishers
+from .testrail_api_reporter.publishers.confluence_sender import ConfluenceSender
+from .testrail_api_reporter.publishers.email_sender import EmailSender
+from .testrail_api_reporter.publishers.slack_sender import SlackSender
 # Utils
+from .testrail_api_reporter.utils.reporter_utils import upload_image
